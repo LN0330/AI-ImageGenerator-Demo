ad795b926a3a8b303636d990fccd3c36528f8711
Neo <Neo@DESKTOP>
2025-03-29 15:56:18 +0800

    update

--- GeneratorAPI/Controllers/GeneratorAPI.cs
+++ GeneratorAPI/Controllers/GeneratorAPI.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 [Route("api/greeting")]
 public class GreetingController : ControllerBase
 {
+    // 請自行更換模型路徑
     private static readonly string sdPath = Path.Combine(Directory.GetCurrentDirectory(), "stable-diffusion\\stable-diffusion.cpp\\build\\bin\\Release", "sd.exe");
     private static readonly string ModelPath = Path.Combine(Directory.GetCurrentDirectory(), "stable-diffusion", "stable_diffusion-ema-pruned-v2-1_768.q8_0.gguf");
     private static readonly string OutputImagePath = Path.Combine(Directory.GetCurrentDirectory(), "stable-diffusion\\stable-diffusion.cpp\\build\\bin\\Release", "output.png");
@@ -11,10 +12,9 @@ public class GreetingController : ControllerBase
     [HttpPost("generate-image")]
     public async Task<IActionResult> GenerateImage([FromBody] UserPrompt request)
     {
+        // --steps 圖片精緻度，數值越大生成時間越長，圖片品質越高 (default 20)
         string arguments = $"-m {ModelPath} -p \"{request.Prompt}\" --steps 20 -o \"{OutputImagePath}\"";
-
-        // 打印要執行的命令
-        Console.WriteLine($"Executing command: {sdPath} {arguments}");
+        Console.WriteLine($"Executing command: {sdPath} {arguments}"); // debug用
 
         if (System.IO.File.Exists(OutputImagePath))
         {
@@ -25,16 +25,19 @@ public class GreetingController : ControllerBase
         {
             FileName = sdPath,
             Arguments = arguments,
-            RedirectStandardOutput = false, // 不捕獲標準輸出
+            RedirectStandardOutput = false,
             UseShellExecute = false,
             CreateNoWindow = true
         };
 
         try
         {
-            // 使用非同步方式啟動，不阻塞主線程
-            var process = new Process { StartInfo = processStartInfo };
-            process.Start();
+            // 圖片生成較久會導致API請求超時，透過 Task.Run 使用非同步啟動進程，避免阻塞主線程
+            await Task.Run(() =>
+            {
+                var process = new Process { StartInfo = processStartInfo };
+                process.Start();
+            });
 
             // 返回狀態，表示圖像生成已開始，等待完成
             return Ok(new { message = "正在生成圖像..." });
@@ -44,6 +47,7 @@ public class GreetingController : ControllerBase
             return StatusCode(500, new { error = ex.Message });
         }
     }
+
 
     [HttpGet("check-image")]
     public async Task<IActionResult> CheckImage()
